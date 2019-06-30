249b10367e1c51137f0a3ba4ee257a20b563d9e2
rajesh_kumar1 <rajesh.kumar1@spglobal.com>
2019-06-29 18:40:12 -0600

    Added One more line

--- ModulesExternal/car.py
+++ ModulesExternal/car.py
@@ -9,4 +9,6 @@ def car_info(make, model):
     print("My car is Infiniti")
     print("My car is Infiniti - FX35")
     print("My car is Infiniti - FX35 - 2011")
+    print("My car is Infiniti - FX35 - 2011 - New Line")
+
 
