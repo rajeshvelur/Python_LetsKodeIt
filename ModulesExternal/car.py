e3adbb103ce25710396e1a1a090e3d9d7a181051
rajesh_kumar1 <rajesh.kumar1@spglobal.com>
2019-06-29 18:18:02 -0600

    added new line to car.py

---
+++ ModulesExternal/car.py
@@ -0,0 +1,9 @@
+"""
+this is our own module
+"""
+
+def car_info(make, model):
+    print("Make of this car is" + make)
+    print("Model of this car is " + model)
+
+    print("My car is Infiniti")
